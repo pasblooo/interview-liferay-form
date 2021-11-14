574bb0954fef95b7228fcc38be3634e622f23164
Pablo Mantas <pablomantas@gmail.com>
2021-11-14 21:51:05 +0100

    Updated dependences

--- acceptance/src/test/java/LaunchForm.java
+++ acceptance/src/test/java/LaunchForm.java
@@ -1,7 +1,32 @@
+import org.apache.commons.io.FilenameUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.en.*;
+
 public class LaunchForm {
-    @io.cucumber.java.en.Given("^Open \"([^\"]*)\" browser$")
-    public void openBrowser(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+    public void set_browser_driver(){
+
+    }
+
+    @Given("Open \"<browser>\" browser")
+    public void open_browser(String browser){
+
+    }
+
+
+    public static void main(String[] args){
+
+        // System properties for Chrome Driver
+        String driver_path = "./src/test/resources/drivers/chromedriver.exe";
+
+        System.setProperty("webdriver.chrome.driver", FilenameUtils.separatorsToSystem(driver_path));
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
     }
 }
---
+++ acceptance/target/test-classes/features/launch.feature
@@ -0,0 +1,34 @@
+Feature: Launch Liferay form
+  As a user
+  I want to launch the Liferay form in browser
+
+  Scenario Outline: Launch the liferay form application with different browsers
+    Given Open "<browser>" browser
+    When Load liferay form aplication
+    Then The form has been loaded correctly
+
+    Examples: Launch in <browser>
+      | browser          |
+      | Chrome           |
+      | Safari           |
+      | Edge             |
+      | Firefox          |
+      | InternetExplorer |
+      | Opera            |
+
+
+  Scenario Outline: Launch the liferay form application with different resolutions
+    Given Open "Chrome" browser
+    When Set browser window size to "<width>" width by "<height>" height
+    And Load liferay form aplication
+    Then The form has been loaded correctly
+
+    Examples: Load with <width> x <heigth> size
+      | width | height |
+      | 1920  | 1080   |
+      | 1280  | 800    |
+      | 1024  | 768    |
+      | 640   | 360    |
+      | 2560  | 1440   |
+      | 962   | 601    |
+      | 896   | 414    |
