e1a233583a917348d755b1eb8edb91854e78bd49
Pablo Mantas <pablomantas@gmail.com>
2021-11-15 16:52:12 +0100

    Form filled

--- acceptance/src/test/java/com/form/steps/ManageBrowser.java
+++ acceptance/src/test/java/com/form/steps/ManageBrowser.java
@@ -1,14 +1,11 @@
 package com.form.steps;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Properties;
-import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 
 public class ManageBrowser {
 
-    public static void loadURL(WebDriver webDriver, String url) {
-        webDriver.get(url);
+    public static void loadURL(WebDriver driver, String url) {
+
+        driver.get(url);
     }
 }
